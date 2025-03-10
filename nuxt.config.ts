bff59537fbbc19d9b289dd263f64a0ce33e971e6
salam <salamatbekboev2008@gmail.com>
2025-03-11 01:07:29 +0900

    feat: remove nuxt-auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: [
-    "@sidebase/nuxt-auth",
     "@nuxt/content",
     "@nuxt/ui",
     "@nuxtjs/tailwindcss",
@@ -37,40 +36,12 @@ export default defineNuxtConfig({
       pathPrefix: true,
     },
   ],
-  auth: {
-    isEnabled: true,
-    baseURL: process.env.NUXT_AUTH_ORIGIN,
-    provider: {
-      type: "authjs",
-      trustHost: false,
-      defaultProvider: "google",
-      addDefaultCallbackUrl: true,
-    },
-  },
   app: {
     head: {
       titleTemplate: "%s %separator %siteName",
       templateParams: {
         siteName: "",
         separator: "-",
-      },
-    },
-  },
-  runtimeConfig: {
-    authOrigin: process.env.NUXT_AUTH_ORIGIN,
-    auth: {
-      secret: process.env.NUXT_AUTH_SECRET,
-      google: {
-        id: process.env.NUXT_AUTH_GOOGLE_ID,
-        secret: process.env.NUXT_AUTH_GOOGLE_SECRET,
-      },
-      vk: {
-        id: process.env.NUXT_AUTH_VK_ID,
-        secret: process.env.NUXT_AUTH_VK_SECRET,
-      },
-      yandex: {
-        id: process.env.NUXT_AUTH_YANDEX_ID,
-        secret: process.env.NUXT_AUTH_YANDEX_SECRET,
       },
     },
   },
