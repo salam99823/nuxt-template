ab13f5893289719872fe8a0451a75e462e304798
salam <salamatbekboev2008@gmail.com>
2025-03-12 15:05:22 +0900

    feat: test page and api endpoint

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
+const appConfig = useAppConfig();
+
 useHead({
-  title: useAppConfig().siteName,
+  title: appConfig.siteName,
   titleTemplate: "",
 });
+
+const colors = appConfig.ui.colors;
+const cls = useState(
+  "cls",
+  () => `text-${colors[Math.floor(Math.random() * colors.length)]}-500`,
+);
 </script>
 
 <template>
-  <main />
+  <div>
+    <h1 :class="cls">Template</h1>
+    <NuxtLink to="/" external> refresh </NuxtLink>
+  </div>
 </template>
 
-<style></style>
+<style scoped></style>
